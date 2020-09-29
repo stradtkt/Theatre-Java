8a683451ec6025b78a3857c4a71732637d078ea4
Kevin Stradtman <stradtkt22@gmail.com>
2020-09-29 00:08:19 -0400

    Added more to the main class"

--- src/Theatre.java
+++ src/Theatre.java
@@ -2,7 +2,7 @@ import java.util.*;
 
 public class Theatre {
     private final String theatreName;
-    private List<Seat> seats = new ArrayList<>();
+    public List<Seat> seats = new ArrayList<>();
 
     public Theatre(String theatreName, int numRows, int seatsPerRow) {
         this.theatreName = theatreName;
@@ -41,7 +41,7 @@ public class Theatre {
             System.out.println(seat.getSeatNumber());
         }
     }
-    private class Seat implements Comparable<Seat> {
+    public class Seat implements Comparable<Seat> {
         private final String seatNumber;
         private boolean reserved = false;
         public Seat(String seatNumber) {

4d602f1ccdd93ae1524231a05ef90952fa24d460
Kevin Stradtman <stradtkt22@gmail.com>
2020-09-28 23:49:34 -0400

    Added the binary search

--- src/Main.java
+++ src/Main.java
@@ -2,12 +2,7 @@ public class Main {
     public static void main(String[] args) {
         Theatre theatre = new Theatre("Olympian", 8, 12);
         theatre.getSeats();
-        if(theatre.reserveSeat("H11")) {
-            System.out.println("Please pay");
-        } else {
-            System.out.println("Sorry seat is taken!");
-        }
-        if(theatre.reserveSeat("H11")) {
+        if(theatre.reserveSeat("D12")) {
             System.out.println("Please pay");
         } else {
             System.out.println("Sorry seat is taken!");
--- src/Theatre.java
+++ src/Theatre.java
@@ -18,26 +18,23 @@ public class Theatre {
         return theatreName;
     }
     public boolean reserveSeat(String seatNumber) {
-        Seat requestSeat = new Seat(seatNumber);
-        int foundSeat = Collections.binarySearch(seats, requestSeat, null);
-        if(foundSeat >= 0) {
-            return seats.get(foundSeat).reserve();
-        } else {
-            System.out.println("There is no seat " + seatNumber);
-            return false;
+        int low = 0;
+        int high = seats.size() - 1;
+        while(low <= high) {
+            System.out.print(".");
+            int mid = (low + high) / 2;
+            Seat midVal = seats.get(mid);
+            int cmp = midVal.getSeatNumber().compareTo(seatNumber);
+            if(cmp < 0) {
+                low = mid + 1;
+            } else if(cmp > 0) {
+                high = mid - 1;
+            } else {
+                return seats.get(mid).reserve();
+            }
         }
-//        for(Seat seat : seats) {
-//            System.out.print(".");
-//            if(seat.getSeatNumber().equals(seatNumber)) {
-//                requestSeat = seat;
-//                break;
-//            }
-//        }
-//        if(requestSeat == null) {
-//            System.out.println("There is no seat " + seatNumber);
-//            return false;
-//        }
-//        return requestSeat.reserve();
+        System.out.println("There is no seat " + seatNumber);
+        return false;
     }
     public void getSeats() {
         for(Seat seat : seats) {
